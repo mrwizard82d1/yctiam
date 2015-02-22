2fa6167e658de499417f328e486db81cbb7aa21e
Larry Jones <mrwizard82d1@gmail.com>
2015-02-22 17:58:02 -0600

    Added initial feature spec (failing).

---
+++ TimeIsMoneyBin/TimeIsMoneyBin.Specs/ViewActivities.feature
@@ -0,0 +1,11 @@
+﻿Feature: View activities
+  In order to ensure I report my time accurately
+  As an employee of Scrooge McDuck
+  I want view my activities at work
+
+Scenario: View activities at start of the day
+  Given I have registered myself
+  And I have entered no activities for today
+  When I selecte the "Add Actitivities" page
+  Then I see today's date
+  And I see an empty activies list
---
+++ TimeIsMoneyBin/TimeIsMoneyBin.Specs/ViewActivitiesSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace TimeIsMoneyBin.Specs
+{
+    [Binding]
+    public class ViewActivitiesSteps
+    {
+        [Given(@"I have registered myself")]
+        public void GivenIHaveRegisteredMyself()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I have entered no activities for today")]
+        public void GivenIHaveEnteredNoActivitiesForToday()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I selecte the ""(.*)"" page")]
+        public void WhenISelecteThePage(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I see today's date")]
+        public void ThenISeeTodaySDate()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I see an empty activies list")]
+        public void ThenISeeAnEmptyActiviesList()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
