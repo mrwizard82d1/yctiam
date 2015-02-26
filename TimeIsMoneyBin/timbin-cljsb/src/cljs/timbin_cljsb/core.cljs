2f2fad4ae7f31a5fb0c2f3360030aa67aa02d293
Larry Jones <mrwizard82d1@gmail.com>
2015-02-26 12:42:48 -0600

    Began implementing next-day (broken).

--- TimeIsMoneyBin/timbin-cljsb/src/cljs/timbin_cljsb/core.cljs
+++ TimeIsMoneyBin/timbin-cljsb/src/cljs/timbin_cljsb/core.cljs
@@ -1,3 +1,17 @@
-(ns timbin-cljsb.core)
+(ns timbin-cljsb.core
+  (:require [goog.dom :as dom]))
 
-(.write js/document "<p>Hello, timbin ClojureScript World!</p>")
+(enable-console-print!)
+
+(println "Does this appear in the developers console?")
+
+(defn prev-day []
+  (println "Handling onclick of prevButton")
+  (let [date-control (dom/getElement "forDate")
+        current-date (.-value date-control)]
+    (println current-date)
+    (set! (.-value date-control)
+          (.setDate current-date (dec (.getDate current-date))))))
+
+(defn next-day []
+  (println "Handling onclick of nextButton"))
