06e78bd17db16c0c61ed87dd44e0d251caae4701
Larry Jones <mrwizard82d1@gmail.com>
2015-02-25 22:23:26 -0600

    Began using vsClojure extension.

---
+++ TimeIsMoneyBin/TimeIsMoneyBin/Scripts/timbin.cljs
@@ -0,0 +1,13 @@
+﻿(ns timbin
+  (:require [goog.dom :as dom]))
+
+(enable-console-print!)
+
+(println "Edits to this text should show up in your developer console.")
+
+(defn next-day []
+  (println "In next-day/0"))
+
+(defn prev-day []
+  (println "In prev-day/0"))
+
